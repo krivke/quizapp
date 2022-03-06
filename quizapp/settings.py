0dd02f0318231beb5771a186c55a9b18225a80ce
Dino Krivic <dkrivic22@gmail.com>
2022-03-06 16:16:36 +0100

    Updated Settings

--- quizapp/settings.py
+++ quizapp/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)dn$!@)e#79z5*lq^iy#28ul(omqxbp2l3dpe61d&%ome8iz&a'
+#SECRET_KEY = 'django-insecure-)dn$!@)e#79z5*lq^iy#28ul(omqxbp2l3dpe61d&%ome8iz&a'
+SECRET_KEY = os.enviorn.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["myawsomequizapp.herokuapp.com"]
 
 
 # Application definition
