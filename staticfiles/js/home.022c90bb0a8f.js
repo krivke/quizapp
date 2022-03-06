9965ae03620fcca99814bf1e80bb2915f777f589
Dino Krivic <dkrivic22@gmail.com>
2022-03-06 18:48:52 +0100

    staticfiles2

---
+++ staticfiles/js/home.022c90bb0a8f.js
@@ -0,0 +1,125 @@
+/*HOME PAGE ANIMATIONS */
+anime({
+    targets:".play",
+    opacity:[0,1],
+    duration:8000
+})
+
+anime({
+    targets:".stats",
+    opacity:[0,1],
+    duration:8000
+})
+
+anime({
+    targets:".main-image",
+    opacity:[0,1],
+    duration:8000
+})
+
+/*FIXED SVG */
+var path = document.getElementById("Path_3");
+var path2 = document.getElementById("Path_4");
+var pathLength = path.getTotalLength();
+var pathLength2 = path2.getTotalLength();
+
+//array u svg su crtice i dashevi koji crtaj taj lik u svg-u
+path.style.strokeDasharray = pathLength + " " + pathLength;
+path2.style.strokeDasharray = pathLength2 + " " + pathLength2;
+//offset  - 
+
+if(document.documentElement.scrollTop == 0){
+
+    path.style.strokeDashoffset = pathLength - (pathLength*0.3);
+    path2.style.strokeDashoffset = pathLength2 - (pathLength2*0.3);
+}else{
+
+    path.style.strokeDashoffset = pathLength - (pathLength*((document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight)));
+    path2.style.strokeDashoffset = pathLength2 - (pathLength2*((document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight)));
+   
+}
+
+
+
+window.addEventListener("scroll",function(e){
+    // WHAT % IS IT DOWN
+    var scrollPercent = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+   
+    if(scrollPercent < 0.3){
+        scrollPercent = 0.3;
+    }
+    // length to offset the dashes
+    var drawLength = pathLength * scrollPercent;
+    var drawLength2 = pathLength2 * scrollPercent;
+    
+    // draw in reverse
+    path.style.strokeDashoffset = pathLength - drawLength;
+    path2.style.strokeDashoffset = pathLength2 - drawLength2;
+
+})
+
+
+
+$(document).ready(function(){
+    $(".home-link  .register-link").on("click",function(e){
+        if(this.hash !==""){
+            e.preventDefault()
+            hash = this.hash;
+           
+            $("html,body").animate({
+                scrollTop:$(hash).offset().top
+            },300,function(){
+                window.location.hash = hash;
+            }
+            )
+        }
+    })
+})
+
+/*LOGIN-REGISTER */
+
+
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true,
+  hash:true,
+ 
+});
+
+
+/*SEE MORE */
+var seeMoreBtns = Array.from(document.getElementsByClassName("see-more-button"))
+var notSlicedCategories = document.getElementById("not-sliced-categories")
+seeMoreBtns.forEach(btn=>{
+    btn.addEventListener("click",(e)=>{
+        e.preventDefault();
+
+        if(btn.id == "arrow-down"){
+            btn.classList.add("hidden");
+            btn.nextElementSibling.classList.remove("hidden")
+            notSlicedCategories.style.height = "auto"
+            
+        }else{
+            btn.classList.add("hidden")
+            btn.previousElementSibling.classList.remove("hidden")
+            notSlicedCategories.style.height = "500px"
+        }
+    })
+    console.log(btn.id)
+})
+
+var x = document.getElementById("delete-message")
+x.addEventListener("click",function(e){
+    var message = document.querySelector(".alert-message")
+    message.classList.add("hidden")
+})
+
+//TODO:stats,profile, after 6 hours of inactivity users must regenerate their token(not create another accnount)
+var bars = Array.from(document.getElementsByClassName("progress-bar-inner"))
+
+bars.forEach(bar=>{
+    var percentage = bar.dataset["id"]
+    bar.style.width = percentage
+})
---
+++ staticfiles/js/play.09b4afb02ec6.js
@@ -0,0 +1,162 @@
+var fetchUrl = JSON.parse(document.getElementById("url").textContent)
+var fetchUrl = fetchUrl.replace("\u0026","&")
+var choices = Array.from(document.getElementsByClassName("choice-text"))
+var question = document.getElementById("question")
+var btns = Array.from(document.getElementsByClassName("btn-choose"))
+
+var CORRECT_ANSWER = ""
+var ACCEPTING_ANSWERS = true
+var CURRENT_CATEGORY = ""
+var CURRENT_QUESTION = ""
+
+var localStorage = window.localStorage
+
+console.log(fetchUrl)
+function getToken(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+var csrftoken = getToken('csrftoken');
+//ONE LINER TO CHECK LOCALSTORAGE SIZE - za sa oko 4KB / 5MB
+//var _lsTotal=0,_xLen,_x;for(_x in localStorage){ if(!localStorage.hasOwnProperty(_x)){continue;} _xLen= ((localStorage[_x].length + _x.length)* 2);_lsTotal+=_xLen; console.log(_x.substr(0,50)+" = "+ (_xLen/1024).toFixed(2)+" KB")};console.log("Total = " + (_lsTotal / 1024).toFixed(2) + " KB");
+
+
+function hidden_loader(length){
+    var loader = document.getElementById("loader");
+    loader.classList.add("hidden")
+    question.classList.remove("hidden")
+
+    for(let i=0;i<length;i++){
+        btns[i].classList.remove("hidden")
+    }  
+}
+
+function fetchForDatabase(category,text,correct_answer,solved){
+    var url = "http://127.0.0.1:8000/add_question/"
+    var data = {
+        category:category,
+        text:text,
+        answer:correct_answer,
+        solved:solved,
+    }
+
+    fetch(url,{
+        method:"POST",
+        headers:{
+          
+            "Content-Type":"application/json",
+            "X-CSRFToken":csrftoken
+        },
+        body:JSON.stringify(data)
+    }).then(response=>{return response.json()})
+    .then(data=>{
+        console.log("Success: ",data)
+    })
+    .catch(error=>{
+        console.log("Error: ",error)
+    })
+}   
+
+
+
+function fetchQuestion(url){
+    fetch(url).then(response=>{
+        if(!response.ok){
+            throw new Error("Could not reach website!")
+        }
+        return response.json()}
+    )
+    .then((data)=>{
+      
+       
+        var questionArray = data["results"][0]
+
+        if(questionArray == undefined){
+            var message = document.getElementById("noMoreQ")
+            message.classList.remove("hidden")
+            return
+        }
+
+        var answers = [...questionArray.incorrect_answers]
+        //random number where correct answer will live
+        var random_number =  Math.floor(Math.random()*3) + 1
+        //adding correct_answer in a random place in a answers list
+        answers.splice(random_number-1,0,questionArray.correct_answer)
+        
+        question.textContent = decodeURIComponent(questionArray.question)
+        var questionCategory = decodeURIComponent(questionArray.category)
+        
+        CORRECT_ANSWER = decodeURIComponent(questionArray.correct_answer)
+        CURRENT_CATEGORY = questionCategory
+        CURRENT_QUESTION = decodeURIComponent(questionArray.question)
+
+        for (var i=0, n=answers.length; i < n; ++i){
+            btns[i].innerHTML = decodeURIComponent(answers[i])
+            
+        }
+        //set all of them to hidden after every question so it can show only T/F  buttons
+        for(let i=0;i<4;i++){
+            btns[i].classList.add("hidden")
+        }
+
+        hidden_loader(answers.length);
+
+        ACCEPTING_ANSWERS = true
+
+    })
+    .catch((error)=>{
+        console.log("New Error: ",error)
+    })
+}
+
+
+
+fetchQuestion(fetchUrl)
+
+
+btns.forEach(btn=>{
+    btn.addEventListener("click",(e)=>{
+        //prevent users to click all choices
+        if(!ACCEPTING_ANSWERS){ return }
+
+        var selectedChoice = e.target
+        if(selectedChoice.textContent == CORRECT_ANSWER){
+                var classToApply = "correct"
+                fetchForDatabase(CURRENT_CATEGORY,CURRENT_QUESTION,CORRECT_ANSWER,true)
+            }else{
+                var classToApply = "incorrect"
+                fetchForDatabase(CURRENT_CATEGORY,CURRENT_QUESTION,CORRECT_ANSWER,false)
+            }
+        selectedChoice.classList.add(classToApply)
+        
+        ACCEPTING_ANSWERS = false
+          
+        /*ako samo stavis setTimeout(fetchQuestions(fetchUrl),1000) - nece radit jel treba 
+        izgledati ovak sa parametrima
+        */
+        //on every click fetch new question and hide classes to Apply when clicked
+        setTimeout(()=>{
+            selectedChoice.classList.remove(classToApply);
+            fetchQuestion(fetchUrl);
+        },1000,fetchUrl)     
+    })    
+})
+
+/*PERHAPS user localStorage for completion of categories */
+/*TODO: na svakih 10% dobijaju neku novu medalju koja je na njihovim profilima
+        za svaku kategoriju imaju medalje
+*/
+
+
+
