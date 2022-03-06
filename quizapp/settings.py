0d761252b89c520ee0eac33d778622dbb7765604
Dino Krivic <dkrivic22@gmail.com>
2022-03-06 18:51:05 +0100

    ad

--- quizapp/settings.py
+++ quizapp/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_HOST = "myawsomequizapp.herokuapp.com/" if not DEBUG else ""
+
 STATIC_URL = "/static/"
 
 
